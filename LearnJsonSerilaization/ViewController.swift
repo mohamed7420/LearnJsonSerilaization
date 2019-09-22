b47bfe542b9313adc75f90c62d0d971d63b5ba09
mohamedosama2074 <iosengks74@gmail.com>
2019-09-22 21:32:17 +0200

    Parsing  simple local json using JSONSerilization

--- LearnJsonSerilaization/ViewController.swift
+++ LearnJsonSerilaization/ViewController.swift
@@ -12,9 +12,58 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
+  let jsonFormat = """
+
+    {
+    "name" : "Ahmed",
+    "Age"  : "21",
+    "email" : "mo7med9878@gmail.com"
     }
 
+    """.data(using: .utf8)!
+        
+        
+        if let jsonSerilization = try? JSONSerialization.jsonObject(with: jsonFormat, options: .allowFragments) as? [String:Any]
+        {
+            
+            if let user = User(dictionary: jsonSerilization)
+            {
+                
+                print(user.email)
+                
+            }
+           
+            
+        }
+    }
+    
 
+
+}
+
+
+struct User {
+    
+    var name:String
+    var Age:String
+    var email:String
+    
+    
+    init?(dictionary:[String:Any]) {
+        
+        guard let name = dictionary["name"] ,
+            let age = dictionary["Age"] ,
+            let email = dictionary["email"]
+            else {return nil}
+        
+        self.name = name as! String
+        self.Age = age as! String
+        self.email = email as! String
+        
+        
+    }
+    
+    
 }
 
